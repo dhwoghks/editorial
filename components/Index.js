65c5dc7ce01a29eafb85dd603553901e02924673
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2025-03-05 20:20:10 +0900

    비포>애프터

--- components/Index.js
+++ components/Index.js
@@ -193,7 +193,7 @@ const Index = () => {
             </div>
 
             {/* Scripts */}
-            <Script src="/assets/js/jquery.min.js" strategy="beforeInteractive" />
+            <Script src="/assets/js/jquery.min.js" strategy="afterInteractive" />
             <Script src="/assets/js/browser.min.js" strategy="afterInteractive" />
             <Script src="/assets/js/breakpoints.min.js" strategy="afterInteractive" />
             <Script src="/assets/js/util.js" strategy="afterInteractive" />
