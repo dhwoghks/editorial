05815b5759c0ef78e46fa9bfdd2a224715ee9e51
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2025-03-17 13:51:31 +0900

    img 태그 써도 잘 돼서 그냥 이거 씀씀

--- components/generic.js
+++ components/generic.js
@@ -23,13 +23,8 @@ const Generic = () => {
                                 <h1>Generic</h1>
                             </header>
 
-                            <span className="image main"><Image 
-                                            src="/images/pic11.jpg" 
-                                            alt="" 
-                                            fill={true}
-                                            sizes="(max-width: 736px) 100vw, (max-width: 1280px) 50vw, 33vw"
-                                            style={{ objectFit: 'cover' }}
-                                        />
+                            <span className="image main">
+                            <img src="images/pic11.jpg" alt="" />
                             </span>
                             
                             <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
