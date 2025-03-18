a53528545ee6f961d495df77b990da5325b6cb88
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2025-03-18 14:52:20 +0900

    elements page 만듦 에러 못잡음

---
+++ app/elements/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Elements from '/components/Elements'; // 헤더 임포트
+
+export default function ElementsPage() {
+    return (
+        <div>
+            <Elements></Elements>
+        </div>
+    )
+}
